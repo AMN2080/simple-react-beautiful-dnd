b55ded559aef945c0082844d984c4cc3d725d8b2
AMN2080 <alireza3205@gmail.com>
2024-03-01 17:11:53 +0330

    fix Board

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -1,72 +1,122 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { DragDropContext } from "react-beautiful-dnd"; 
 import TaskCol from "../TaskCol";
 
+const taskData = {
+  taskCols: [
+    {
+      id: '0',
+      title: 'To Do',
+      taskIds: [0,1,2,3,5]
+    },
+    {
+      id: '1',
+      title: 'Doing',
+      taskIds: [4]
+    },
+    {
+      id: '2',
+      title: 'Test',
+      taskIds: [6,8,10]
+    },
+    {
+      id: '3',
+      title: 'Done',
+      taskIds: [7,9]
+    }
+  ],
+  tasks: [
+    {
+    "id": 0,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    },
+    {
+    "id": 1,
+    "title": "delectus aut autem",
+    },
+    {
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    },
+    {
+    "id": 3,
+    "title": "fugiat veniam minus",
+    },
+    {
+    "id": 4,
+    "title": "et porro tempora",
+    },
+    {
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    },
+    {
+    "id": 6,
+    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+    },
+    {
+    "id": 7,
+    "title": "illo expedita consequatur quia in",
+    },
+    {
+    "id": 8,
+    "title": "quo adipisci enim quam ut ab",
+    },
+    {
+    "id": 9,
+    "title": "molestiae perspiciatis ipsa",
+    },
+    {
+    "id": 10,
+    "title": "illo est ratione doloremque quia maiores aut",
+    }
+  ]
+}
+
 const Board = () => {
-  const [completed, setCompleted] = useState([]);
-  const [incomplete, setIncomplete] = useState([]);
+  const [taskCols, setTaskCols] = useState(taskData.taskCols);
+  const tasks = taskData.tasks;
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => res.json())
-      .then((json) => {
-        setCompleted(json.filter((task) => task.completed));
-        setIncomplete(json.filter((task) => !task.completed));
-      })
-  }, []);
+  //پاکش کن آخر
+  // const [test, setTest] = useState()
 
-  const findItemById = (id, items) => {
-    return items.find(item => item.id === id);
-  }
-  
-  const removeItemById = (id, items) => {
-    return items.filter(item => item.id !== id);
-  }  
-
-  const handleDradEnd = (result) => {
+  const handleDragEnd = (result) => {
     const { destination, source, draggableId } = result;
-
-    if(source.draggableId === destination.draggableId){
+  
+    if(!destination || (source.droppableId === destination.droppableId && source.index === destination.index)){
       return;
     }
+  
+    const newTaskCols = taskCols.map((Col) => {
+      let newTaskIds = [...Col.taskIds];
+      if(Col.id === source.droppableId){
+        newTaskIds.splice(source.index, 1);
+      }
+      if(Col.id === destination.droppableId){
+        newTaskIds.splice(destination.index, 0, Number(draggableId));
+      }
+      
+      return {...Col, taskIds: newTaskIds};
+    })
+    
+    setTaskCols(newTaskCols);
+  }
 
-    // remove
-    if(source.draggableId === 2){
-      setCompleted(removeItemById(draggableId, completed))
-    } else {
-      setIncomplete(removeItemById(draggableId, incomplete))
-    }
-
-    // get
-    const task = findItemById(draggableId, [...incomplete, ...completed]);
-
-    // add
-    if(destination.droppableId === 2){
-      // completed: true اینطوری هم میشه انگار
-      setCompleted([{...task, completed: !task.completed}, ...completed])
-    } else {
-      setIncomplete([{...task, completed: !task.completed}, ...incomplete])
-    }
+  const taskPointer = (taskIds) => {
+    return tasks.filter((task) => taskIds.includes(task.id))
   }
 
   return (
-    <DragDropContext onDragEnd={handleDradEnd}>
+    <DragDropContext onDragEnd={handleDragEnd}>
       <div className="h-screen flex justify-center items-center gap-5">
-        <TaskCol
-          title="To Do"
-          tasks={incomplete}
-          id="1"
-        />
-        <TaskCol
-          title="Doing"
-          tasks={completed}
-          id="2"
-        />
-        <TaskCol
-          title="Done"
-          tasks={[]}
-          id="3"
-        />
+        {taskCols.map((col) => (
+          <TaskCol
+            key={col.id}
+            title={col.title}
+            tasks={taskPointer(col.taskIds)}
+            id={String(col.id)}
+          />
+        ))}
       </div>
     </DragDropContext>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,6 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode></React.StrictMode> یارو گفت ممکنه مشکل ساز شه
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// <React.StrictMode></React.StrictMode> یارو گفت ممکنه مشکل ساز شه
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
