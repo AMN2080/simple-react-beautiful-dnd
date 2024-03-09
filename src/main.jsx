fcc5194ff4fe5c300442e1b2ebcab692f53c2263
Alireza Mohamadzade <95294495+AMN2080@users.noreply.github.com>
2024-03-09 20:25:06 +0330

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,4 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// <React.StrictMode></React.StrictMode> یارو گفت ممکنه مشکل ساز شه
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
